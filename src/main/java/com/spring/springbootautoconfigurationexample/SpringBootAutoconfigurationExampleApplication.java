04030ade95adefddd8bc665e653ab8e8204cf4ec
samukma <nguyensam576@gmail.com>
2021-11-19 10:28:48 +0700

    create proj

---
+++ src/main/java/com/spring/springbootautoconfigurationexample/SpringBootAutoconfigurationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springbootautoconfigurationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigurationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoconfigurationExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springbootautoconfigurationexample/SpringBootAutoconfigurationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootautoconfigurationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoconfigurationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
